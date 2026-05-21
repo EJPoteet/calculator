aeeb1bfcdf1cad3b522ffc43ec637b46755a42c3
Emma Jean Poteet <271843@carmax.com>
2026-05-21 10:06:30 -0400

    Add logic for updating operand values

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let firstNum = 0;
 let secondNum = 0;
 let operator = '';
+const buttons = document.querySelectorAll('.button');
+const resultsDisplay = document.querySelector('#resultsDisplay');
 
 function add(a, b) {
     return a + b;
@@ -31,4 +33,23 @@ function calculate(firstNum, secondNum, operator) {
     }
 }
 
-// console.log(calculate(4, 2, '/'));
+function updateNum(newValue, whichOperand) {
+    if (whichOperand === 1) {
+        firstNum = Number(`${firstNum}${newValue}`);
+        resultsDisplay.textContent = `${firstNum}`;
+        return;
+    } else if (whichOperand === 2) {
+        secondNum = Number(`${secondNum}${newValue}`);
+        resultsDisplay.textContent = `${secondNum}`;
+        return;
+    }
+}
+
+buttons.forEach(button => {
+    const buttonValue = button.textContent;
+    button.addEventListener('click', () => {
+        //update this logic to later to call update functions
+        firstNum = buttonValue;
+        console.log(firstNum);
+    })
+})
