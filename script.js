16909d1f66c45bd86bb5f430e7c7922db39ffac2
Emma Jean Poteet <271843@carmax.com>
2026-05-21 15:54:07 -0400

    Tie html elements into calculator functionality.
    This commit adds functions to update number variables and ties the
    javascript functionality into the html with eventListeners.

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 let firstNum = 0;
 let secondNum = 0;
 let operator = '';
+let updateFirstNum = false;
+let updateSecondNum = false;
+const operators = ['x', '/', '-', '+']
+const nums = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 const buttons = document.querySelectorAll('.button');
 const resultsDisplay = document.querySelector('#resultsDisplay');
 
@@ -33,23 +37,51 @@ function calculate(firstNum, secondNum, operator) {
     }
 }
 
+//consider separating out the logic that updates the display to another function
 function updateNum(newValue, whichOperand) {
     if (whichOperand === 1) {
         firstNum = Number(`${firstNum}${newValue}`);
         resultsDisplay.textContent = `${firstNum}`;
+        updateFirstNum = true;
         return;
     } else if (whichOperand === 2) {
         secondNum = Number(`${secondNum}${newValue}`);
-        resultsDisplay.textContent = `${secondNum}`;
+        updateSecondNum = true;
         return;
     }
 }
 
 buttons.forEach(button => {
     const buttonValue = button.textContent;
+
     button.addEventListener('click', () => {
-        //update this logic to later to call update functions
-        firstNum = buttonValue;
-        console.log(firstNum);
+        if (!operator && nums.includes(buttonValue)) {
+            updateNum(buttonValue, 1);
+        } else if (updateFirstNum && !updateSecondNum && operators.includes(buttonValue)) {
+            operator = buttonValue;
+            resultsDisplay.textContent = `${firstNum} ${operator}`;
+        } else if (updateSecondNum && operators.includes(buttonValue)) {
+            firstNum = calculate(firstNum, secondNum, operator);
+            operator = buttonValue
+            resultsDisplay.textContent = `${firstNum} ${operator}`;
+            secondNum = 0;
+            updateSecondNum = false;
+        } else if (operator && nums.includes(buttonValue)) {
+            updateNum(buttonValue, 2);
+            resultsDisplay.textContent = `${firstNum} ${operator} ${secondNum}`;
+        } else if (operator && buttonValue === '=') {
+            firstNum = calculate(firstNum, secondNum, operator);
+            resultsDisplay.textContent = `${firstNum}`;
+            operator = '';
+            secondNum = 0;
+            updateSecondNum = false;
+        } else if (buttonValue === 'Clear') {
+            firstNum = 0;
+            secondNum = 0;
+            operator = '';
+            updateFirstNum = false;
+            updateSecondNum = false;
+            resultsDisplay.textContent = '0';
+        }
     })
 })
